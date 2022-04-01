fbcbcfd24fbdaef000418ce38021fd96c01e959a
Yik chen <296452965@qq.com>
2022-04-01 23:50:58 +0800

    添加了基础模板，运用模板继承修改了主页模板，添加了错误模板

--- app.py
+++ app.py
@@ -42,6 +42,17 @@ def forge():
     click.echo('Forge done.')
 
 
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # 返回模板和状态码
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
@@ -55,6 +66,5 @@ class Movie(db.Model):
 
 @app.route("/")
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
